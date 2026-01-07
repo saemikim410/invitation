6064aea49f4c166a42f31b7995e9a4e92815927b
saemikim <saebomkim410@gmail.com>
2026-01-07 22:19:44 +0900

    text

--- src/pages/V1/Etc.js
+++ src/pages/V1/Etc.js
@@ -6,7 +6,7 @@ function Etc() {
     <div className="container bc-pink" id="etc_div" data-aos="fade-up">
         <div className='title'>기타 안내 사항</div>
         <div className='sub_title'>✔ 연회장 안내</div>
-        <div className='cover__place-detail'>1층에 위치해 있습니다.</div>
+        <div className='cover__place-detail'>지하 1층에 위치해 있습니다.</div>
         <div className='cover__place-detail'>11시 20분부터 13시 20분까지 식사 가능합니다.</div>
         <div className='cover__place-detail'>3~10세는 어린이 식권이 배부됩니다.</div>
         <div className='cover__place-detail'>(3세 미만은 무료)</div>
